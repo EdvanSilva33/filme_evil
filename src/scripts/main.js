b884b5d8e425e120694a910bc70a442439caa2e3
edvan silva <edvansilvapereira@gmail.com>
2023-05-22 10:51:39 -0300

    ajuste no site

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,22 +1,83 @@
 const btn = document.getElementById("btn");
 const modal = document.getElementById("modal");
 const closeBtn = document.getElementsByClassName("close")[0];
-// const accordionItems = document.querySelectorAll('infor__box__item');
+
 
 
 document.addEventListener('DOMContentLoaded', function() {
-  const questions = document.querySelectorAll(["data-infor-control"]);
+  const acoordeon = document.querySelectorAll('[data-infor-control]');
 
-  for (let i = 0; i < questions.length; i++) {
-    questions[i].addEventListener('click', abreOuFechaAcoordeon);
+  const heroSelector = document.querySelector('.hero');
+  const alturaHero = heroSelector.clientHeight;
+
+  window.addEventListener('scroll', function() {
+    const posicaoScroll = window.scrollY;
+
+    if (posicaoScroll < alturaHero){
+      ocultaHeader();
+    }else{
+      apareceHeader();
+    }
+  })
+
+  for (let i = 0; i < acoordeon.length; i++) {
+    acoordeon[i].addEventListener('click', abreOuFechaAcoordeon);
   }
 
 })
 
- function abreOuFechaAcoordeon(elemento){
+function ocultaHeader(){
+  const header = document.querySelector('header');
+  header.classList.add('header--is-hidden')
+
+}
+function apareceHeader(){
+  const header = document.querySelector('header');
+  header.classList.remove('header--is-hidden');
+
+}
+
+ function abreOuFechaAcoordeon(e){
   const classe = 'infor__box__item--is-open';
-  console.log(elemento);
+  console.log(e);
+  const elementoPai = e.target.parentNode;
+
+
+  elementoPai.classList.toggle(classe)
  }
+
+  //secão slideshow
+
+ let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("container__Slides");
+  let dots = document.getElementsByClassName("infor__image__item__thumb");
+  let captionText = document.getElementById("text");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
 
 
 
@@ -36,15 +97,6 @@ closeBtn.onclick = function() {
   }, 300);      // Tempo correspondente à duração da transição em CSS
 }
 
-
-// accordionItems.forEach(item => {
-//   const header = item.querySelector('.infor__box__item__cubo');
-
-//   header.addEventListener('click', () => {
-//     // Alternar classe "ativo" para mostrar/ocultar conteúdo
-//     item.classList.toggle('ativo')
-
-// })
  
 
 
